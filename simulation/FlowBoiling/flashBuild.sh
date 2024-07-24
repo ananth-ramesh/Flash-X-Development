b8b2aa13d4133aabb76947551a69c036c040ca84
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2024-07-24 12:22:40 -0500

    Initial commmit

---
+++ simulation/FlowBoiling/flashBuild.sh
@@ -0,0 +1,15 @@
+# cache the value of current working directory
+NodeDir=$(realpath .)
+
+# run Flash-X setup
+cd $FLASHX_HOME && echo Flash-X HEAD is at $(git rev-parse --short HEAD)
+cd $FLASHX_HOME && git checkout $FlashSha && ./setup $FlashOptions
+
+# compile the simulation and copy files
+cd $FLASHX_HOME/object && make && cp flashx $NodeDir/ && cp flash.par $NodeDir/ && cp *hdf5_htr* $NodeDir/
+
+# copy AMReX configuration
+cd $AMREX2D_HOME/lib/pkgconfig && cp amrex.pc $NodeDir/
+
+# chdir into node directory and do clean up
+cd $NodeDir #&& rm -rf $FLASHX_HOME/object
---
+++ simulation/FlowBoiling/flashOptions.sh
@@ -0,0 +1,10 @@
+# cache the value of current working directory
+
+#FlashSha="5aa7289e"
+
+FlashOptions="incompFlow/FlowBoiling -auto -maxblocks=100 -2d -nxb=16 -nyb=16 \
+              +amrex +parallelIO -site=$SiteHome +incomp -tomlfile=$JobWorkDir/job.input"
+
+if [ $Profile = True ]; then
+	FlashOptions="$FlashOptions +hpctoolkit"
+fi
