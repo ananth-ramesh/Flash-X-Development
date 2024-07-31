79c61fca6b4f650a8ddd6789564b13b2b225b515
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2024-07-31 17:33:48 -0500

    temp fixes

--- simulation/BubblyFlow/flashOptions.sh
+++ simulation/BubblyFlow/flashOptions.sh
@@ -1,6 +1,6 @@
 # cache the value of current working directory
 
-FlashSha="adhruv/toml-configuration"
+FlashSha="adhruv/curvature-smoothing"
 
 FlashOptions="incompFlow/BubblyFlow -auto -maxblocks=100 -2d -nxb=16 -nyb=16 \
               +amrex +parallelIO -site=$SiteHome +incomp -tomlfile=$JobWorkDir/job.input"
--- simulation/FlowBoiling/flashOptions.sh
+++ simulation/FlowBoiling/flashOptions.sh
@@ -1,6 +1,6 @@
 # cache the value of current working directory
 
-FlashSha="adhruv/toml-configuration"
+FlashSha="adhruv/curvature-smoothing"
 
 FlashOptions="incompFlow/FlowBoiling -auto -maxblocks=100 -2d -nxb=16 -nyb=16 \
               +amrex +parallelIO -site=$SiteHome +incomp -tomlfile=$JobWorkDir/job.input"
