52bcf3c1526603856a18649777c5148cfb902f29
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2024-08-02 02:29:31 -0500

    updates

--- simulation/PoolBoiling/flashOptions.sh
+++ simulation/PoolBoiling/flashOptions.sh
@@ -1,6 +1,6 @@
 # cache the value of current working directory
 
-FlashSha="adhruv/curvature-smoothing"
+FlashSha="adhruv/mph-updates"
 
 FlashOptions="incompFlow/FlowBoiling -auto -maxblocks=100 -2d -nxb=16 -nyb=16 \
               +amrex +serialIO -site=$SiteHome +incomp -tomlfile=$JobWorkDir/job.input"
