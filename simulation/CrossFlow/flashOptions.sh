7ad8132a1fca0e7c9a5ff194f0c171d80e89084c
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2024-08-07 19:38:48 -0500

    Add CrossFlow simulation

---
+++ simulation/CrossFlow/flashOptions.sh
@@ -0,0 +1,6 @@
+# cache the value of current working directory
+
+FlashSha="adhruv/mph-updates"
+
+FlashOptions="incompFlow/CrossFlow -auto -maxblocks=500 \
+              -2d -nxb=16 -nyb=16 +amrex +serialIO -site=$SiteHome +incomp"
