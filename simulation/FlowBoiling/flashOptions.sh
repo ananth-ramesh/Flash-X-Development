6b4d806218897b24f5fbbdfe7157e9e29f5442bb
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2024-08-18 22:31:03 -0500

    update pb sim

--- simulation/FlowBoiling/flashOptions.sh
+++ simulation/FlowBoiling/flashOptions.sh
@@ -1,9 +1,9 @@
 # cache the value of current working directory
 
-FlashSha="adhruv/mph-updates"
+FlashSha="adhruv/incomp-flux-corr"
 
-FlashOptions="incompFlow/FlowBoiling -auto -maxblocks=100 -2d -nxb=16 -nyb=16 \
-              +amrex +parallelIO -site=$SiteHome +incomp -tomlfile=$JobWorkDir/job.input"
+FlashOptions="incompFlow/FlowBoiling -auto -maxblocks=300 -2d -nxb=8 -nyb=8 \
+              +amrex +parallelIO +nolwf -site=$SiteHome +incomp -tomlfile=$JobWorkDir/job.input"
 
 if [ $Profile = True ]; then
 	FlashOptions="$FlashOptions +hpctoolkit"
