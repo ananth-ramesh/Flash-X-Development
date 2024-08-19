cac24fd63e68c133665c627710f3126f41fcefe8
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2024-08-18 22:04:53 -0500

    pool boiling amr

--- simulation/PoolBoiling/flashOptions.sh
+++ simulation/PoolBoiling/flashOptions.sh
@@ -1,6 +1,6 @@
 # cache the value of current working directory
 
-FlashSha="adhruv/mph-updates"
+FlashSha="adhruv/incomp-flux-corr"
 
 FlashOptions="incompFlow/PoolBoiling -auto -maxblocks=100 -2d -nxb=16 -nyb=16 \
-              +amrex +serialIO -site=$SiteHome +incomp -tomlfile=$JobWorkDir/job.input"
+              +amrex +nolwf +serialIO -site=$SiteHome +incomp -tomlfile=$JobWorkDir/job.input"
