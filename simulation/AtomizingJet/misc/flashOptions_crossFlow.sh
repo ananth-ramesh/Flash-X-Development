b09b1c41ae31dd2c2ed8a42ce2c77a5a82d28025
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2024-08-20 15:28:48 -0500

    update misc in AtomizingJet

---
+++ simulation/AtomizingJet/misc/flashOptions_crossFlow.sh
@@ -0,0 +1,6 @@
+# cache the value of current working directory
+
+FlashSha="adhruv/incomp-flux-corr"
+
+FlashOptions="incompFlow/AtomizingJet -auto -maxblocks=1000 \
+              -3d -nxb=8 -nyb=8 -nzb=8 +amrex +nolwf +serialIO -site=$SiteHome +incomp"
