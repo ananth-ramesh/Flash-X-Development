3db5bf37a1a720633695a84daabfb96bc478eef2
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2024-08-25 19:10:59 -0500

    updates for airfoil optimization case

---
+++ simulation/ChannelFlow/misc/flashOptions_rae2882.sh
@@ -0,0 +1,8 @@
+# cache the value of current working directory
+
+FlashSha="adhruv/incomp-flux-corr"
+
+FlashOptions="incompFlow/ChannelFlow -auto \
+              -maxblocks=1000 -2d -nxb=8 -nyb=8 \
+              -with-unit=physics/ImBound/ImBoundMain \
+              +incomp +nolwf +amrex +serialIO -site=$SiteHome"
--- software/ann/setupANN.sh
+++ software/ann/setupANN.sh
@@ -2,10 +2,10 @@
 
 # Setup AMReX
 if [ ! -d "ANN" ]; then
-	git clone git@github.com:Box-Tools/ANN.git && cd ANN
+	git clone git@github.com:Box-Tools/ANN-Fortran.git ANN && cd ANN
 
 	# checkout desired branch
-	git checkout 36df1a5
+	git checkout 7ea8384
 
 else
 	cd ANN
