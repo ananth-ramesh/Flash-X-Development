06bc49756a7a9cf5652c2b720df5a50abedfe06c
Akash <adhruv@polaris-login-02.hsn.cm.polaris.alcf.anl.gov>
2024-10-30 01:13:47 +0000

    make changes for polaris

--- software/flashx/setupFlashX.sh
+++ software/flashx/setupFlashX.sh
@@ -1,5 +1,5 @@
 # Setup Flash-X
 if [ ! -d "Flash-X" ]; then
 	git clone git@github.com:Flash-X/Flash-X --branch main Flash-X && cd Flash-X
-        git submodule update --init --recursive
+        git submodule update --init source/Grid/GridMain/AMR/Paramesh4/PM4_package
 fi
